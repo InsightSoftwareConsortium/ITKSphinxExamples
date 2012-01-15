b150487ba68a61b92c58462f4af61b684062dc02
Matt McCormick <matt.mccormick@kitware.com>
2012-01-15 11:29:36 -0500

    Fix tarball input image path specification.
    We replace CMAKE_CURRENT_BINARY_DIR with CMAKE_CURRENT_SOURCE_DIR when we
    generate the tarballs.  This way we do have to put the input images in the build
    directory of the tarball, and the user can nuke the contents of their build
    directory as usual, and tests will past the next time around.

--- scripts/create_tarball.py
+++ scripts/create_tarball.py
@@ -15,9 +15,11 @@ import sys
 import tarfile
 import zipfile
 
+
 if len(sys.argv) < 3:
     print('usage: ' + sys.argv[0] + ' <example_name> <SPHINX_DESTINATION>')
     sys.exit(1)
+
 
 # Collect our directories.
 cwd = os.getcwd()
@@ -26,17 +28,36 @@ example_dir = os.path.join(cwd, example_name)
 example_idx = example_dir.index('Examples')
 html_output_dir = os.path.join(sys.argv[2], 'html', example_dir[example_idx:])
 
+
 # Copy files to support web editting to the html output.
 files_to_copy_to_html_output = []
+files_to_copy_to_html_output.append(os.path.join(example_dir, 'CMakeLists.txt'))
 files_to_copy_to_html_output.extend(glob.glob(os.path.join(example_dir, '*.cxx')))
 files_to_copy_to_html_output.extend(glob.glob(os.path.join(example_dir, '*.rst')))
-files_to_copy_to_html_output.append(os.path.join(example_dir, 'CMakeLists.txt'))
 for path in files_to_copy_to_html_output:
     shutil.copy2(path, html_output_dir)
 
+
 # Tarballs for users to download from the website.
 files_for_tarball = files_to_copy_to_html_output
+
 # Add a directory to build the project.
+build_dir = os.path.join(example_dir, 'build')
+if not os.path.exists(build_dir):
+    os.mkdir(build_dir)
+files_for_tarball.append(build_dir)
+
+# Substitude CMAKE_CURRENT_BINARY_DIR for CMAKE_CURRENT_SOURCE_DIR in the
+# tarball CMakeLists.txt.  This so the input images can be found.
+tarball_cmakelist = os.path.join(example_dir, 'CMakeLists.txt.tarball')
+with open(tarball_cmakelist, 'w') as new_list_file:
+    with open(os.path.join(example_dir, 'CMakeLists.txt'), 'r') as old_list_file:
+        for line in old_list_file:
+            newline = line.replace('CMAKE_CURRENT_BINARY_DIR', 'CMAKE_CURRENT_SOURCE_DIR')
+            new_list_file.write(newline)
+files_for_tarball = files_for_tarball[1:]
+files_for_tarball.append(tarball_cmakelist)
+
 inputs = glob.glob(os.path.join(example_dir, '*.md5'))
 for path in inputs:
     files_for_tarball.append(path[:-4])
@@ -48,10 +69,9 @@ with tarfile.open(os.path.join(html_output_dir, example_name + '.tar.gz'), \
         # Put the inputs and outputs into the build directory because the test
         # will not be able to find them otherwise.
         basename = os.path.basename(tarinfo.name)
-        if tarinfo.name[-4:] != '.cxx' and tarinfo.name[-4:] != '.rst' \
-                and basename != 'CMakeLists.txt' and basename != 'build':
-                    head, tail = os.path.split(tarinfo.name)
-                    tarinfo.name = os.path.join(head, 'build', tail)
+        if basename == 'CMakeLists.txt.tarball':
+                head, tail = os.path.split(tarinfo.name)
+                tarinfo.name = os.path.join(head, 'CMakeLists.txt')
         return tarinfo
     for path in files_for_tarball:
         tarfile.add(path, filter=strip_path)
@@ -64,8 +84,7 @@ with zipfile.ZipFile(os.path.join(html_output_dir, example_name + '.zip'), \
         # Put the inputs and outputs into the build directory because the test
         # will not be able to find them otherwise.
         basename = os.path.basename(arcname)
-        if arcname[-4:] != '.cxx' and arcname[-4:] != '.rst' \
-                and basename != 'CMakeLists.txt' and basename != 'build':
-                    head, tail = os.path.split(arcname)
-                    arcname = os.path.join(head, 'build', tail)
+        if basename == 'CMakeLists.txt.tarball':
+                head, tail = os.path.split(arcname)
+                arcname = os.path.join(head, 'CMakeLists.txt')
         zipfile.write(path, arcname)
