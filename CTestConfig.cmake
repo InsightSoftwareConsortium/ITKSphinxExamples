6f76b6eab3ef4d95e4ff4956a2ffeab7eaf32c5d
Matt McCormick (thewtex) <matt@mmmccormick.com>
2012-01-18 23:26:28 -0500

    Add CTestConfig.cmake.

---
+++ CTestConfig.cmake
@@ -0,0 +1,7 @@
+set(CTEST_PROJECT_NAME "ITKExamples")
+set(CTEST_NIGHTLY_START_TIME "1:00:00 UTC")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "mmmccormick.com")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=ITKExamples")
+set(CTEST_DROP_SITE_CDASH TRUE)
