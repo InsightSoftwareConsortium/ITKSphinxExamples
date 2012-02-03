900be81301cbca0aa9b6212b424d46b531eeb7ac
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2012-02-03 11:55:34 -0500

    Works locally (download first all doxygen documentation)

--- superbuild/External-ITKDoxygen.cmake
+++ superbuild/External-ITKDoxygen.cmake
@@ -1,5 +1,5 @@
 ExternalProject_Add( ITKDoxygen
-  URL http://dl.dropbox.com/u/15135983/InsightDoxygen.tag.tar.bz2
+  URL http://dl.dropbox.com/u/15135983/itk-doxygen.tar.bz2
   SOURCE_DIR ITKDoxygen
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
