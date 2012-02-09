e53476722cf92749cd49168b7642dc5b29fbc071
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2012-02-09 04:58:23 -0500

    fix SobelEdgeDetectionImageFilter.cxx

--- src/Examples/Filtering/ImageFeature/SobelEdgeDetectionImageFilter/SobelEdgeDetectionImageFilter.cxx
+++ src/Examples/Filtering/ImageFeature/SobelEdgeDetectionImageFilter/SobelEdgeDetectionImageFilter.cxx
@@ -1,39 +1,39 @@
-// #include "itkImage.h
-// #include "itkImageFileReader.h
-// #include "itkImageFileWriter.h
+#include "itkImage.h"
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
 #include "itkSobelEdgeDetectionImageFilter.h"
 
 int main( int argc, char* argv[] )
 {
-  // if( argc != 3 )
-  //   {
-  //   std::cerr << Usage: << std::endl;
-  //   std::cerr << argv[0];
-  //   std::cerr << "<InputFileName> <OutputFileName>;
-  //   std::cerr << std::endl;
-  //   return EXIT_FAILURE;
-  //   }
+  if( argc != 3 )
+    {
+    std::cerr << Usage: << std::endl;
+    std::cerr << argv[0];
+    std::cerr << "<InputFileName> <OutputFileName>;
+    std::cerr << std::endl;
+    return EXIT_FAILURE;
+    }
 
-  // const unsigned int Dimension = 2;
+  const unsigned int Dimension = 2;
 
-  // typedef unsigned char                      PixelType;
-  // typedef itk::Image< PixelType, Dimension > ImageType;
+  typedef unsigned char                      PixelType;
+  typedef itk::Image< PixelType, Dimension > ImageType;
 
-  // typedef itk::ImageFileReader< ImageType >  ReaderType;
-  // ReaderType::Pointer reader = ReaderType::New();
-  // reader->SetInputFileName( argv[1] );
-  // reader->Update();
+  typedef itk::ImageFileReader< ImageType >  ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetInputFileName( argv[1] );
+  reader->Update();
 
-  // typedef itk::SobelEdgeDetectionImageFilter<> FilterType;
-  // FilterType::Pointer filter = FilterType::New();
-  // filter->SetInput( reader->GetOutput() );
-  // filter->Update();
+  typedef itk::SobelEdgeDetectionImageFilter<> FilterType;
+  FilterType::Pointer filter = FilterType::New();
+  filter->SetInput( reader->GetOutput() );
+  filter->Update();
 
-  // typedef itk::ImageFileWrite< ImageType > WriteType;
-  // WriterType::Pointer writer = WriterType::New();
-  // writer->SetFileName( argv[2] );
-  // writer->SetInput( filter->GetOutput() );
-  // writer->Update();
+  typedef itk::ImageFileWrite< ImageType > WriteType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName( argv[2] );
+  writer->SetInput( filter->GetOutput() );
+  writer->Update();
 
-return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
