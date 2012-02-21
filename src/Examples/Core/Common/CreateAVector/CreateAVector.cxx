e8a5abc5f4d376f12b945e60de848a6badab6361
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2012-02-21 09:12:54 -0500

    CreateAVector

---
+++ src/Examples/Core/Common/CreateAVector/CreateAVector.cxx
@@ -0,0 +1,17 @@
+#include "itkVector.h"
+
+int main(int, char*[])
+{
+  const unsigned int Dimension = 3;
+  typedef double CoordType;
+
+  typedef itk::Vector<CoordType, Dimension> VectorType;
+
+  VectorType v;
+  v[0] = 1.0;
+  v[1] = 2.0;
+  v[2] = 3.0;
+  std::cout << "v: " << v << std::endl;
+
+  return EXIT_SUCCESS;
+}
