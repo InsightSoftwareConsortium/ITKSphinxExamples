c0b0f4ecd6c31e7656462430da7a190df9fe5be9
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2012-02-21 08:36:14 -0500

    Add GetTypeBasicInformation

---
+++ src/Examples/Core/Common/GetTypeBasicInformation/GetTypeBasicInformation.cxx
@@ -0,0 +1,39 @@
+// #include "itkImage.h"
+// #include "itkImageFileReader.h"
+// #include "itkImageFileWriter.h"
+#include "itkNumericTraits.h"
+
+int main( int argc, char* argv[] )
+{
+  // if( argc != 3 )
+  //   {
+  //   std::cerr << "Usage: "<< std::endl;
+  //   std::cerr << argv[0];
+  //   std::cerr << "<InputFileName> <OutputFileName>";
+  //   std::cerr << std::endl;
+  //   return EXIT_FAILURE;
+  //   }
+
+  // const unsigned int Dimension = 2;
+
+  // typedef unsigned char                      PixelType;
+  // typedef itk::Image< PixelType, Dimension > ImageType;
+
+  // typedef itk::ImageFileReader< ImageType >  ReaderType;
+  // ReaderType::Pointer reader = ReaderType::New();
+  // reader->SetFileName( argv[1] );
+  // reader->Update();
+
+  // typedef itk::NumericTraits< ImageType, ImageType > FilterType;
+  // FilterType::Pointer filter = FilterType::New();
+  // filter->SetInput( reader->GetOutput() );
+  // filter->Update();
+
+  // typedef itk::ImageFileWriter< ImageType > WriterType;
+  // WriterType::Pointer writer = WriterType::New();
+  // writer->SetFileName( argv[2] );
+  // writer->SetInput( filter->GetOutput() );
+  // writer->Update();
+
+  return EXIT_SUCCESS;
+}
---
+++ src/Examples/Core/Common/GetTypeBasicInformation/GetTypeBasicInformation.rst
@@ -0,0 +1,42 @@
+Get Type Basic Information
+==========================
+
+.. index::
+  single: NumericTraits
+
+Synopsis
+--------
+
+
+Get some basic information about a type
+
+
+Results
+-------
+
+.. figure::
+  :scale: 50%
+  :alt: Input image
+
+  Input image
+
+.. figure::
+  :scale: 50%
+  :alt: Output image
+
+  Output image
+
+
+Code
+----
+
+C++
+...
+
+.. literalinclude:: GetTypeBasicInformation.cxx
+
+
+Classes demonstrated
+--------------------
+
+. breathelink: itk::NumericTraits
