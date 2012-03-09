f514af85539c44b300aad24caea0337e2fc556a5
Arnaud Gelas <arnaud_gelas@hms.harvard.edu>
2012-03-09 11:46:24 -0500

    Add baseline image for ApplyAtanImageFilter

--- src/Examples/Filtering/ImageIntensity/ApplyAtanImageFilter/ApplyAtanImageFilter.cxx
+++ src/Examples/Filtering/ImageIntensity/ApplyAtanImageFilter/ApplyAtanImageFilter.cxx
@@ -1,37 +1,46 @@
 #include "itkImage.h"
-#include "itkImageFileReader.h"
+#include "itkRandomImageSource.h"
 #include "itkImageFileWriter.h"
 #include "itkAtanImageFilter.h"
 
 int main( int argc, char* argv[] )
 {
-  if( argc != 3 )
+  if( argc != 2 )
     {
     std::cerr << "Usage: "<< std::endl;
     std::cerr << argv[0];
-    std::cerr << "<InputFileName> <OutputFileName>";
+    std::cerr << " <OutputFileName>";
     std::cerr << std::endl;
     return EXIT_FAILURE;
     }
 
   const unsigned int Dimension = 2;
+  typedef unsigned char InputPixelType;
 
-  typedef unsigned char                      PixelType;
-  typedef itk::Image< PixelType, Dimension > ImageType;
+  typedef itk::Image< InputPixelType, Dimension >  InputImageType;
 
-  typedef itk::ImageFileReader< ImageType >  ReaderType;
-  ReaderType::Pointer reader = ReaderType::New();
-  reader->SetFileName( argv[1] );
-  reader->Update();
+  InputImageType::SizeType size;
+  size.Fill( 100 );
 
-  typedef itk::AtanImageFilter< ImageType, ImageType > FilterType;
+  typedef itk::RandomImageSource< InputImageType > RandomImageSourceType;
+
+  RandomImageSourceType::Pointer randomImageSource =
+    RandomImageSourceType::New();
+  randomImageSource->SetNumberOfThreads(1); // to produce non-random results
+  randomImageSource->SetSize(size);
+  randomImageSource->Update();
+
+  typedef float OutputPixelType;
+  typedef itk::Image< OutputPixelType, Dimension > OutputImageType;
+
+  typedef itk::AtanImageFilter< InputImageType, OutputImageType > FilterType;
   FilterType::Pointer filter = FilterType::New();
-  filter->SetInput( reader->GetOutput() );
+  filter->SetInput( randomImageSource->GetOutput() );
   filter->Update();
 
-  typedef itk::ImageFileWriter< ImageType > WriterType;
+  typedef itk::ImageFileWriter< OutputImageType > WriterType;
   WriterType::Pointer writer = WriterType::New();
-  writer->SetFileName( argv[2] );
+  writer->SetFileName( argv[1] );
   writer->SetInput( filter->GetOutput() );
   writer->Update();
 
--- src/Examples/Filtering/ImageIntensity/ApplyAtanImageFilter/ApplyAtanImageFilter.rst
+++ src/Examples/Filtering/ImageIntensity/ApplyAtanImageFilter/ApplyAtanImageFilter.rst
@@ -14,18 +14,6 @@ Compute the arctangent of each pixel.
 Results
 -------
 
-.. figure::
-  :scale: 50%
-  :alt: Input image
-
-  Input image
-
-.. figure::
-  :scale: 50%
-  :alt: Output image
-
-  Output image
-
 
 Code
 ----
