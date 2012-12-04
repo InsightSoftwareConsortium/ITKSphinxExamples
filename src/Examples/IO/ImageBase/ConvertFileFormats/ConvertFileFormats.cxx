6d5253fd3adb42369122e6a58234f93c89af02ac
Matt McCormick <matt.mccormick@kitware.com>
2012-12-04 00:33:14 -0500

    ENH: Add ConvertFileFormats example.
    Change-Id: Icd7b8cb4eb2760b7bb5f12036110eac43f340eb8

---
+++ src/Examples/IO/ImageBase/ConvertFileFormats/ConvertFileFormats.cxx
@@ -0,0 +1,40 @@
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+
+int main( int argc, char* argv[] )
+{
+  if( argc != 3 )
+    {
+    std::cerr << "Usage: "<< std::endl;
+    std::cerr << argv[0];
+    std::cerr << "<InputFileName> <OutputFileName>";
+    std::cerr << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  const unsigned int Dimension = 2;
+
+  typedef unsigned char                      PixelType;
+  typedef itk::Image< PixelType, Dimension > ImageType;
+
+  typedef itk::ImageFileReader< ImageType >  ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( argv[1] );
+
+  typedef itk::ImageFileWriter< ImageType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName( argv[2] );
+  writer->SetInput( reader->GetOutput() );
+  try
+    {
+    writer->Update();
+    }
+  catch( itk::ExceptionObject & exception )
+    {
+    std::cerr << "Error during Update() " << std::endl;
+    std::cerr << exception << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  return EXIT_SUCCESS;
+}
