363170a122ddfb035c57c906d001c30c20a95258
Matt McCormick <matt.mccormick@kitware.com>
2013-08-14 02:47:46 -0400

    ENH: Add VTK superbuild support.
    Change-Id: Ia6021415f3bb8c91da5e916cf97ca54b96c7a546

--- Superbuild/External-ITK.cmake
+++ Superbuild/External-ITK.cmake
@@ -6,6 +6,16 @@ if( NOT ITK_TAG )
   set( ITK_TAG "d73c465ed4b65d1c81e00b4e68ef4c859a1f7e10" )
 endif()
 
+set( _vtk_args )
+set( _vtk_depends )
+if( VTK_DIR OR ITKExamples_USE_VTK )
+  set( _vtk_args "-DVTK_DIR:PATH=${VTK_DIR}"
+    -DModule_ITKVtkGlue:BOOL=ON
+    -DModule_ITKLevelSetsv4Visualization:BOOL=ON
+    )
+  set( _vtk_depends VTK )
+endif()
+
 ExternalProject_Add( ITK
   GIT_REPOSITORY "${git_protocol}://itk.org/ITK.git"
   GIT_TAG "${ITK_TAG}"
@@ -18,11 +28,10 @@ ExternalProject_Add( ITK
     -DBUILD_EXAMPLES:BOOL=OFF
     -DBUILD_TESTING:BOOL=OFF
     -DITK_BUILD_ALL_MODULES:BOOL=ON
-#    -DVTK_DIR:PATH=${VTK_DIR}
-#    -DModule_ITKVtkGlue:BOOL=ON
     -DITK_USE_REVIEW:BOOL=ON
+    ${_vtk_args}
   INSTALL_COMMAND ""
-#  DEPENDS VTK
+  DEPENDS ${_vtk_depends}
 )
 
 set(ITK_DIR ${CMAKE_BINARY_DIR}/ITK-build)
