2f5900b2ef1d1194d98f76a721d9f65bdba9132e
Arnaud Gelas <arnaudgelas@gmail.com>
2014-05-12 18:26:06 +0200

    COMP: Fix for failing ApplyAccumulateImageFilter baseline comparison test
    The ApplyAccumulateImageFilterTestBaselineComparison fails because the test
    which needs to be run before it (to produce the Output0.mha file) is run AFTER
    the baseline comparison.  The problem is that the PROPERTIES DEPENDS is set as
    ${example_name}Test, which will not work with tests named like
    ApplyAccumulateImageFilterTest${i}
    
    The CMake function compare_to_baseline has been modified such that now we can
    easily add dependencies in between test, or options.
    
    Here is the new API:
    
    compare_to_baseline(
      [TEST_NAME myAwesomeExampleBaselineComparison1] // this argument is optional and meant
                                                      // to be used when there would be several
                                                      // baseline comparison tests based on one given
                                                      // example
      EXAMPLE_NAME myAwesomeExampleName
      BASELINE_PREFIX myBaselineImagePrefix
      [TEST_IMAGE_PREFIX myAwesomeExampleOutputImagePrefix] // this argument is optional and meant
                                                            // to be used when there would be several
                                                            // baseline comparison tests based on one given
                                                            // example
      [OPTIONS myListOfOptions]
      [DEPENDS myListOfDependencies]
    )
    
    Co-Authored-By: Michka Popoff <michkapopoff@gmail.com>
    
    Change-Id: Ib3a2301a6136f8d7a946716b9454ef3b076cf8e5

---
+++ src/ITKExamplesMacro.cmake
@@ -0,0 +1,138 @@
+include( CMakeParseArguments )
+
+# A high level target that the individual targets depend on.
+#add_custom_target( CreateTarballs ALL
+#  ${CMAKE_COMMAND} -E echo "Done creating individual example tarballs..."
+#)
+
+# Macro for adding an example.
+#
+# Most of the CMake logic for the example should live in the examples
+# CMakeLists.txt.  That file should be standalone for building the example.
+# This macro is intended to be instantiated in the CMakeLists.txt one directory
+# above where the example exists.  It does things like creating the example
+# tarball's in the example output directory, and custom targets for building
+# the Sphinx documentation for an individual example.  The Sphinx target will be
+# called ${example_name}Doc.
+macro( add_example example_name )
+  if( BUILD_DOCUMENTATION )
+    if( SPHINX_HTML_OUTPUT )
+      add_custom_target( ${example_name}Tarball
+        COMMAND ${PYTHON_EXECUTABLE} ${ITKExamples_SOURCE_DIR}/Utilities/CreateTarball.py
+          ${example_name} ${SPHINX_DESTINATION}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+        COMMENT "Creating tarballs for ${example_name}"
+        DEPENDS copy_sources
+        )
+      add_dependencies( CreateTarballs ${example_name}Tarball )
+    endif()
+  endif()
+
+  # Process the example's CMakeLists.txt
+  add_subdirectory( ${example_name} )
+endmacro()
+
+
+# Macro if the corresponding module is enabled when ITK was built
+# Pass in the module directory path.  Also pass the module name as the second
+# argument if it is different than ITK${_dir}.
+macro( add_subdirectory_if_module_enabled _dir )
+  set( _module_name ITK${_dir} )
+  if( "${ARGV1}" STRGREATER "" )
+    set( _module_name ${ARGV1} )
+  endif()
+  list( FIND ITK_MODULES_ENABLED ${_module_name} _have )
+
+  if( NOT ${_have} EQUAL "-1" )
+    add_subdirectory( ${_dir} )
+  endif()
+endmacro()
+
+# Creates a test that compares the output image of an example to its baseline
+# image.
+#
+# Usage:
+# compare_to_baseline(
+#   [TEST_NAME myAwesomeExampleBaselineComparison1] // this argument is optional and meant
+#                                                   // to be used when there would be several
+#                                                   // baseline comparison tests based on one given
+#                                                   // example
+#   EXAMPLE_NAME myAwesomeExampleName
+#   BASELINE_PREFIX myBaselineImagePrefix
+#   [TEST_IMAGE_PREFIX myAwesomeExampleOutputImagePrefix] // this argument is optional and meant
+#                                                   // to be used when there would be several
+#                                                   // baseline comparison tests based on one given
+#                                                   // example
+#   [OPTIONS myListOfOptions]
+#   [DEPENDS myListOfDependencies]
+# )
+#
+# Note:
+# * By default TEST_NAME is set to ${EXAMPLE_NAME}TestBaselineComparison
+function( compare_to_baseline )
+
+  set( options
+  )
+
+  set( oneValueArgs
+    EXAMPLE_NAME
+    TEST_IMAGE_PREFIX
+    BASELINE_PREFIX
+    TEST_NAME
+  )
+
+  set( multiValueArgs
+    DEPENDS
+    OPTIONS
+  )
+
+  cmake_parse_arguments( LOCAL_COMPARISON
+    "${options}"
+    "${oneValueArgs}"
+    "${multiValueArgs}"
+    ${ARGN}
+  )
+
+  if( NOT LOCAL_COMPARISON_EXAMPLE_NAME )
+    message( FATAL_ERROR "Syntax error in use of compare_to_baseline: EXAMPLE_NAME is required" )
+  endif()
+
+  if( NOT LOCAL_COMPARISON_BASELINE_PREFIX )
+    message( FATAL_ERROR "Syntax error in use of compare_to_baseline: BASELINE_PREFIX is required" )
+  endif()
+
+  set( depends ${LOCAL_COMPARISON_EXAMPLE_NAME}Test )
+  if( LOCAL_COMPARISON_DEPENDS )
+    set( depends ${LOCAL_COMPARISON_DEPENDS} )
+  endif()
+
+  file( GLOB baseline_image ${CMAKE_CURRENT_SOURCE_DIR}/${LOCAL_COMPARISON_EXAMPLE_NAME}/${LOCAL_COMPARISON_BASELINE_PREFIX}.* )
+  string( REPLACE .md5 "" baseline_image "${baseline_image}" )
+  get_filename_component( baseline_image_file "${baseline_image}" NAME )
+  set( baseline_image "${CMAKE_CURRENT_BINARY_DIR}/${LOCAL_COMPARISON_EXAMPLE_NAME}/${baseline_image_file}" )
+
+  if( NOT LOCAL_COMPARISON_TEST_IMAGE_PREFIX )
+    string( REPLACE Baseline "" test_image "${baseline_image}" )
+  else()
+    file( GLOB test_image ${CMAKE_CURRENT_BINARY_DIR}/${LOCAL_COMPARISON_EXAMPLE_NAME}/${LOCAL_COMPARISON_TEST_IMAGE_PREFIX}.* )
+    list( GET test_image 0 test_image )
+  endif()
+
+  set( test_name )
+  if( NOT LOCAL_COMPARISON_TEST_NAME )
+    set( test_name ${LOCAL_COMPARISON_EXAMPLE_NAME}TestBaselineComparison )
+  else()
+    set( test_name ${LOCAL_COMPARISON_TEST_NAME} )
+  endif()
+
+  add_test( NAME ${test_name}
+    COMMAND "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/ImageCompareCommand"
+      --test-image "${test_image}"
+      --baseline-image "${baseline_image}"
+      ${LOCAL_COMPARISON_OPTIONS}
+  )
+
+ set_tests_properties( ${test_name}
+    PROPERTIES DEPENDS ${depends}
+    )
+endfunction()
