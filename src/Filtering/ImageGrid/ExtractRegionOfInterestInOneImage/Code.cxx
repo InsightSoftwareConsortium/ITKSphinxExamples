8c5ad98687fa66fd23fe16baa101ca82d6f01623
Arnaud Gelas <arnaudgelas@gmail.com>
2014-11-26 14:33:23 +0100

    Add one example about RegionOfInterestImageFilter
    Change-Id: Ib1e38cd3d9373e5ac28ac0eee8665b2df8ab734a

---
+++ src/Filtering/ImageGrid/ExtractRegionOfInterestInOneImage/Code.cxx
@@ -0,0 +1,72 @@
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkRegionOfInterestImageFilter.h"
+
+int main( int argc, char* argv[] )
+{
+  if( argc != 7 )
+    {
+    std::cerr << "Usage: "<< std::endl;
+    std::cerr << argv[0];
+    std::cerr << " <InputFileName> <OutputFileName>"
+    std::cerr << " <start x> <end x> <start y> <end y>";
+    std::cerr << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  const char * inputFileName = argv[1];
+  const char * outputFileName = argv[2];
+
+  const itk::IndexValueType startx =
+    static_cast< itk::IndexValueType >( atoi( argv[3] ) );
+  const itk::IndexValueType endx =
+    static_cast< itk::IndexValueType >( atoi( argv[4] ) );
+
+  const itk::IndexValueType starty =
+    static_cast< itk::IndexValueType >( atoi( argv[5] ) );
+  const itk::IndexValueType endy =
+    static_cast< itk::IndexValueType >( atoi( argv[6] ) );
+
+  const unsigned int Dimension = 2;
+
+  typedef unsigned char                      PixelType;
+  typedef itk::Image< PixelType, Dimension > ImageType;
+
+  typedef itk::ImageFileReader< ImageType >  ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( inputFileName );
+
+  ImageType::IndexType start;
+  start[0] = startx;
+  start[1] = starty;
+
+  ImageType::IndexType end;
+  end[0] = endx;
+  end[1] = endy;
+
+  ImageType::RegionType region;
+  region.SetIndex( start );
+  region.SetUpperIndex( end );
+
+  typedef itk::RegionOfInterestImageFilter< ImageType, ImageType >
+    FilterType;
+  FilterType::Pointer filter = FilterType::New();
+  filter->SetInput( reader->GetOutput() );
+  filter->SetRegionOfInterest( region );
+
+  typedef itk::ImageFileWriter< ImageType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetFileName( outputFileName );
+  writer->SetInput( filter->GetOutput() );
+  try
+    {
+    writer->Update();
+    }
+  catch( itk::ExceptionObject & error )
+    {
+    std::cerr << "Error: " << error << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  return EXIT_SUCCESS;
+}
---
+++ src/Filtering/ImageGrid/ExtractRegionOfInterestInOneImage/Documentation.rst
@@ -0,0 +1,41 @@
+Extract Region Of Interest In One Image
+=======================================
+
+.. index::
+   single: RegionOfInterestImageFilter
+
+Synopsis
+--------
+
+Extract a given Region Of Interest (ROI) in a given image
+
+
+Results
+-------
+
+.. figure:: Gourds.png
+  :scale: 50%
+  :alt: Input image
+
+  Input image
+
+.. figure:: OutputBaseline.png
+  :scale: 50%
+  :alt: Output image
+
+  Output image
+
+
+Code
+----
+
+C++
+...
+
+.. literalinclude:: Code.cxx
+
+
+Classes demonstrated
+--------------------
+
+.. breathelink:: itk::RegionOfInterestImageFilter
