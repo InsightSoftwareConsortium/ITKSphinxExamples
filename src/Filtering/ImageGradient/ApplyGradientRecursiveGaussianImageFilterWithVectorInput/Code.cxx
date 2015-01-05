2bf9674f3d816203baec835f30d479356e2a68fc
Michka Popoff <michkapopoff@gmail.com>
2015-01-05 17:44:42 +0100

    ENH: Add new example with GradientRecursiveGaussianImageFilter
    The input is made of a two dimensional image with vector type (2 components).
    Serves as example to answer a question in ticket ITK-3330.
    
    Change-Id: I9b59c9482bb0452274711981e748847d1f8d913f

---
+++ src/Filtering/ImageGradient/ApplyGradientRecursiveGaussianImageFilterWithVectorInput/Code.cxx
@@ -0,0 +1,112 @@
+#include "itkImageFileReader.h"
+#include "itkImageFileWriter.h"
+#include "itkImage.h"
+#include "itkGradientRecursiveGaussianImageFilter.h"
+#include "itkRescaleIntensityImageFilter.h"
+#include "itkVectorIndexSelectionCastImageFilter.h"
+#include "itkVectorMagnitudeImageFilter.h"
+#include "itkInvertIntensityImageFilter.h"
+#include "itkComposeImageFilter.h"
+#include "itkCastImageFilter.h"
+
+int main( int argc, char* argv[] )
+{
+  if( argc != 6 )
+    {
+    std::cerr << "Usage: "<< std::endl;
+    std::cerr << argv[0];
+    std::cerr << " <InputFileName> <OutputFileName1X> <OutputFileName1Y> <OutputFileName2X> <OutputFileName2Y>";
+    std::cerr << std::endl;
+    return EXIT_FAILURE;
+    }
+
+  const char * inputFileName =    argv[1];
+  const char * outputFileName1X = argv[2];
+  const char * outputFileName1Y = argv[3];
+  const char * outputFileName2X = argv[4];
+  const char * outputFileName2Y = argv[5];
+
+  const char *filenames [4];
+  filenames[0] = outputFileName1X;
+  filenames[1] = outputFileName1Y;
+  filenames[2] = outputFileName2X;
+  filenames[3] = outputFileName2Y;
+
+  const unsigned int ImageDimension =  2;
+  const unsigned int VectorDimension = 2;
+  const unsigned int CovDimension =    4;
+
+  typedef unsigned char                                           PixelType;
+  typedef itk::Image< PixelType, ImageDimension >                 ImageType;
+  typedef double                                                  DoublePixelType;
+  typedef itk::Image< DoublePixelType, ImageDimension >           DoubleImageType;
+  typedef itk::Vector< DoublePixelType, VectorDimension >         VecPixelType;
+  typedef itk::Image< VecPixelType, ImageDimension >              VecImageType;
+  typedef itk::CovariantVector< DoublePixelType, CovDimension >   CovPixelType;
+  typedef itk::Image< CovPixelType, ImageDimension >              CovImageType;
+
+  typedef itk::ImageFileReader< ImageType > ReaderType;
+  ReaderType::Pointer reader = ReaderType::New();
+  reader->SetFileName( inputFileName );
+
+  // Invert the input image
+  typedef itk::InvertIntensityImageFilter< ImageType, ImageType > InvertType;
+  InvertType::Pointer inverter = InvertType::New();
+  inverter->SetInput( reader->GetOutput() );
+
+  // Cast the image to double type.
+  typedef itk::CastImageFilter< ImageType, DoubleImageType > CasterType;
+  CasterType::Pointer caster = CasterType::New();
+  CasterType::Pointer caster2 = CasterType::New();
+
+  // Create an image, were each pixel has 2 values: first value is the value
+  // coming from the input image, second value is coming from the inverted
+  // image
+  typedef itk::ComposeImageFilter< DoubleImageType, VecImageType > ComposeType;
+  ComposeType::Pointer composer = ComposeType::New();
+  caster->SetInput( reader->GetOutput() );
+  composer->SetInput( 0, caster->GetOutput() );
+  caster2->SetInput( inverter->GetOutput() );
+  composer->SetInput( 1, caster2->GetOutput() );
+
+  // Apply the gradient filter on the two images, this will return and image
+  // with 4 values per pixel: two X and Y gradients
+  typedef itk::GradientRecursiveGaussianImageFilter< VecImageType, CovImageType > FilterType;
+  FilterType::Pointer filter = FilterType::New();
+  filter->SetInput( composer->GetOutput() );
+
+  // Set up the filter to select each gradient
+  typedef itk::VectorIndexSelectionCastImageFilter< CovImageType, DoubleImageType > IndexSelectionType;
+  IndexSelectionType::Pointer indexSelectionFilter = IndexSelectionType::New();
+  indexSelectionFilter->SetInput( filter->GetOutput() );
+
+  // Rescale for png output
+  typedef itk::RescaleIntensityImageFilter< DoubleImageType, ImageType > RescalerType;
+  RescalerType::Pointer rescaler = RescalerType::New();
+  rescaler->SetOutputMinimum( itk::NumericTraits< PixelType >::min() );
+  rescaler->SetOutputMaximum( itk::NumericTraits< PixelType >::max() );
+  rescaler->SetInput( indexSelectionFilter->GetOutput() );
+
+  typedef itk::ImageFileWriter< ImageType > WriterType;
+  WriterType::Pointer writer = WriterType::New();
+  writer->SetInput( rescaler->GetOutput() );
+
+  // Write the X and Y images
+  for( int i = 0; i<4 ;++i )
+  {
+    indexSelectionFilter->SetIndex( i );
+    writer->SetFileName( filenames[i] );
+
+    try
+    {
+      writer->Update();
+    }
+    catch( itk::ExceptionObject & error )
+    {
+      std::cerr << "Error: " << error << std::endl;
+      return EXIT_FAILURE;
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
---
+++ src/Filtering/ImageGradient/ApplyGradientRecursiveGaussianImageFilterWithVectorInput/Code.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+#=========================================================================
+#
+#  Copyright Insight Software Consortium
+#
+#  Licensed under the Apache License, Version 2.0 (the "License")
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http:*www.apache.org/licenses/LICENSE-2.0.txt
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#=========================================================================
+
+import sys
+import itk
+
+from distutils.version import StrictVersion as VS
+if VS(itk.Version.GetITKVersion()) < VS("4.8.0"):
+    print("ITK 4.8.0 is required (see example documentation).")
+    sys.exit(1)
+
+if len(sys.argv) != 6:
+    print(
+        "Usage: " + sys.argv[0] +
+        " [InputFileName] [OutputFileName1X] [OutputFileName1Y]" +
+        " [OutputFileName2X] [OutputFileName2Y]")
+    sys.exit(1)
+
+inputFileName = sys.argv[1]
+outputFileName1X = sys.argv[2]
+outputFileName1Y = sys.argv[3]
+outputFileName2X = sys.argv[4]
+outputFileName2Y = sys.argv[5]
+
+filenames = [
+    outputFileName1X,
+    outputFileName1Y,
+    outputFileName2X,
+    outputFileName2Y]
+
+ImageDimension = 2
+VectorDimension = 2
+CovDimension = 4
+
+PixelType = itk.UC
+ImageType = itk.Image[PixelType, ImageDimension]
+DoublePixelType = itk.D
+DoubleImageType = itk.Image[DoublePixelType, ImageDimension]
+VecPixelType = itk.Vector[DoublePixelType, VectorDimension]
+VecImageType = itk.Image[VecPixelType, ImageDimension]
+CovPixelType = itk.CovariantVector[DoublePixelType, CovDimension]
+CovImageType = itk.Image[CovPixelType, ImageDimension]
+
+ReaderType = itk.ImageFileReader[ImageType]
+reader = ReaderType.New()
+reader.SetFileName(inputFileName)
+
+# Invert the input image
+InvertType = itk.InvertIntensityImageFilter[ImageType, ImageType]
+inverter = InvertType.New()
+inverter.SetInput(reader.GetOutput())
+
+# Cast the image to double type.
+CasterType = itk.CastImageFilter[ImageType, DoubleImageType]
+caster = CasterType.New()
+caster2 = CasterType.New()
+
+# Create an image, were each pixel has 2 values: first value is the value
+# coming from the input image, second value is coming from the inverted
+# image
+ComposeType = itk.ComposeImageFilter[DoubleImageType, VecImageType]
+composer = ComposeType.New()
+caster.SetInput(reader.GetOutput())
+composer.SetInput(0, caster.GetOutput())
+caster2.SetInput(inverter.GetOutput())
+composer.SetInput(1, caster2.GetOutput())
+
+# Apply the gradient filter on the two images, this will return and image
+# with 4 values per pixel: two X and Y gradients
+FilterType = itk.GradientRecursiveGaussianImageFilter[
+    VecImageType, CovImageType]
+gradientfilter = FilterType.New()
+gradientfilter.SetInput(composer.GetOutput())
+
+# Set up the filter to select each gradient
+IndexSelectionType = itk.VectorIndexSelectionCastImageFilter[
+    CovImageType, DoubleImageType]
+indexSelectionFilter = IndexSelectionType.New()
+indexSelectionFilter.SetInput(gradientfilter.GetOutput())
+
+# Rescale for png output
+RescalerType = itk.RescaleIntensityImageFilter[DoubleImageType, ImageType]
+rescaler = RescalerType.New()
+rescaler.SetOutputMinimum(itk.NumericTraits[PixelType].min())
+rescaler.SetOutputMaximum(itk.NumericTraits[PixelType].max())
+rescaler.SetInput(indexSelectionFilter.GetOutput())
+
+WriterType = itk.ImageFileWriter[ImageType]
+writer = WriterType.New()
+writer.SetInput(rescaler.GetOutput())
+
+for i in range(4):
+    indexSelectionFilter.SetIndex(i)
+    writer.SetFileName(filenames[i])
+    writer.Update()
