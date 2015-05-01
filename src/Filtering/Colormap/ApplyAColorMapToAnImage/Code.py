37eedbe6a4e38967ee33efed1cd45ad75cb35648
Michka Popoff <michkapopoff@gmail.com>
2015-05-01 15:33:55 +0200

    ENH: Add ApplyColorMap Python example
    The rescaling was removed as it is not needed to
    show how to apply a colormap.
    
    Change-Id: I847ca9b707f48fc6e13ac0750ea6d72f8c38a4e7

---
+++ src/Filtering/Colormap/ApplyAColorMapToAnImage/Code.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+#=========================================================================
+#
+#  Copyright Insight Software Consortium
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+#=========================================================================
+
+import sys
+import itk
+
+if len(sys.argv) != 3:
+    print("Usage: " + sys.argv[0] + " <inputImage> <outputImage>")
+    sys.exit(1)
+
+inputImage = sys.argv[1]
+outputImage = sys.argv[2]
+PixelType = itk.UC
+Dimension = 2
+
+ImageType = itk.Image[PixelType, Dimension]
+
+ReaderType = itk.ImageFileReader[ImageType]
+reader = ReaderType.New()
+reader.SetFileName(inputImage)
+
+RGBPixelType = itk.RGBPixel[PixelType]
+RGBImageType = itk.Image[RGBPixelType, Dimension]
+
+RGBFilterType = itk.ScalarToRGBColormapImageFilter[ImageType, RGBImageType]
+rgbfilter = RGBFilterType.New()
+rgbfilter.SetInput(reader.GetOutput())
+rgbfilter.SetColormap(RGBFilterType.Hot)
+
+WriterType = itk.ImageFileWriter[RGBImageType]
+writer = WriterType.New()
+writer.SetFileName(outputImage)
+writer.SetInput(rgbfilter.GetOutput())
+
+writer.Update()
