b5c16763eab014b74565bcc3b7b1935b759dbe3d
Michka Popoff <michkapopoff@gmail.com>
2016-10-02 13:20:02 +0200

    ENH: Add BoundingBoxOfAPointSet Python example
    Change-Id: I74e760dbc7e6a1d7c8d4405602315f9ffd547a90

---
+++ src/Core/Common/BoundingBoxOfAPointSet/Code.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+#==========================================================================
+#
+#   Copyright Insight Software Consortium
+#
+#   Licensed under the Apache License, Version 2.0 (the "License")
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#==========================================================================*/
+
+import itk
+
+CoordType = itk.F
+Dimension = 3
+
+MeshTraits = itk.DefaultStaticMeshTraits[CoordType, Dimension, Dimension]
+PointSetType = itk.PointSet[CoordType, Dimension, MeshTraits]
+
+pointSet = PointSetType.New()
+points = pointSet.GetPoints()
+
+# Create points
+p0 = itk.Point[CoordType, Dimension]()
+p1 = itk.Point[CoordType, Dimension]()
+p2 = itk.Point[CoordType, Dimension]()
+
+p0[0] = 0.0
+p0[1] = 0.0
+p0[2] = 0.0
+p1[0] = 0.1
+p1[1] = 0.0
+p1[2] = 0.0
+p2[0] = 0.0
+p2[1] = 0.1
+p2[2] = 0.0
+
+points.InsertElement(0, p0)
+points.InsertElement(1, p1)
+points.InsertElement(2, p2)
+
+VecContType = itk.VectorContainer[itk.UL, itk.Point[CoordType, Dimension]]
+BoundingBoxType = itk.BoundingBox[itk.UL, Dimension, CoordType, VecContType]
+
+boundingBox = BoundingBoxType.New()
+boundingBox.SetPoints(points)
+boundingBox.ComputeBoundingBox()
+
+print("bounds: " + str(boundingBox.GetBounds()))
+print("center: " + str(boundingBox.GetCenter()))
+print("diagonal length squared: " + str(boundingBox.GetDiagonalLength2()))
--- src/Core/Common/BoundingBoxOfAPointSet/Documentation.rst
+++ src/Core/Common/BoundingBoxOfAPointSet/Documentation.rst
@@ -31,6 +31,12 @@ C++
 .. literalinclude:: Code.cxx
    :lines: 18-
 
+Python
+......
+
+.. literalinclude:: Code.py
+   :lines: 1, 20-
+
 
 Classes demonstrated
 --------------------
