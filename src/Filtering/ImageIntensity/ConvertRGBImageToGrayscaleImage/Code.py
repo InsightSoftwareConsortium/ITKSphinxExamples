04224eb0ff7e3aae38889df6ad45b5340656df0a
Max Smolens <max.smolens@kitware.com>
2017-03-15 13:59:22 -0400

    ENH: Add itk.RGBToLuminanceImageFilter Python example
    Add itk.RGBToLuminanceImageFilter Python example. A C++ example for
    RGBToLuminanceImageFilter already exists next to this new example.
    
    Additionally, remove the entry from ITKRepositoryImport.todo that corresponds to
    this example.
    
    Change-Id: Ib615404ac2b9dcd5c2368ef5a394aa708a0df2d3

---
+++ src/Filtering/ImageIntensity/ConvertRGBImageToGrayscaleImage/Code.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+#==========================================================================
+#
+#   Copyright Insight Software Consortium
+#
+#   Licensed under the Apache License, Version 2.0 (the "License")
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#==========================================================================*/
+
+import sys
+import itk
+
+if len(sys.argv) != 3:
+    print('Usage: ' + sys.argv[0] + ' <InputFileName> <OutputFileName>')
+    sys.exit(1)
+
+inputFileName = sys.argv[1]
+outputFileName = sys.argv[2]
+
+Dimension = 2
+
+ComponentType = itk.UC
+InputPixelType = itk.RGBPixel[ComponentType]
+InputImageType = itk.Image[InputPixelType, Dimension]
+
+OutputPixelType = itk.UC
+OutputImageType = itk.Image[OutputPixelType, Dimension]
+
+reader = itk.ImageFileReader[InputImageType].New()
+reader.SetFileName(inputFileName)
+
+rgbFilter = itk.RGBToLuminanceImageFilter[InputImageType, OutputImageType].New()
+rgbFilter.SetInput(reader.GetOutput())
+
+writer = itk.ImageFileWriter[OutputImageType].New()
+writer.SetFileName(outputFileName)
+writer.SetInput(rgbFilter.GetOutput())
+
+writer.Update()
