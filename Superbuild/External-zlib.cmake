56a76a63e09ffa064f78ea9f227177126e7080a1
Matt McCormick <matt.mccormick@kitware.com>
2018-09-26 15:31:58 -0400

    BUG: ZLib ExternalProject uses ep_common_args

--- Superbuild/External-zlib.cmake
+++ Superbuild/External-zlib.cmake
@@ -9,9 +9,7 @@ ExternalProject_Add(zlib
   BINARY_DIR zlib-build
   INSTALL_DIR zlib-install
   CMAKE_CACHE_ARGS
-    ## CXX should not be needed, but it a cmake default test
-    -DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}
-    -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}
+    ${ep_common_args}
     -DZLIB_MANGLE_PREFIX:STRING=itkexamples_zlib_
     -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
   )
