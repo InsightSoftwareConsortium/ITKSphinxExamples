ab77b59ffee67845d6d5329c471834bd817bf195
Matt McCormick <matt.mccormick@kitware.com>
2018-10-08 12:51:30 -0400

    ENH: Move "-Examples" to the end of the CTest build name

--- azure_dashboard.cmake
+++ azure_dashboard.cmake
@@ -31,7 +31,7 @@ if(NOT CTEST_BUILD_NAME)
     set(pr "")
   endif()
   set(CTEST_BUILD_NAME
-    "$ENV{AGENT_OS}-Examples-Build$ENV{BUILD_BUILDID}${pr}${branch}")
+    "$ENV{AGENT_OS}-Build$ENV{BUILD_BUILDID}${pr}${branch}-Examples")
 endif()
 
 string(TIMESTAMP build_date "%Y-%m-%d")
