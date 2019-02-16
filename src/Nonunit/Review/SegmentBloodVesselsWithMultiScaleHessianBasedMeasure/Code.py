81f95f5313b3669791e33a269544eeb6dcf27529
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2019-02-16 16:01:50 -0500

    ENH: Add SegmentBloodVesselsWithMultiScaleHessianBasedMeasure Python code.
    Add SegmentBloodVesselsWithMultiScaleHessianBasedMeasure Python example
    code.

---
+++ src/Nonunit/Review/SegmentBloodVesselsWithMultiScaleHessianBasedMeasure/Code.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+# Copyright Insight Software Consortium
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+
+import itk
+from distutils.version import StrictVersion as VS
+if VS(itk.Version.GetITKVersion()) < VS("5.0.0"):
+    print("ITK 5.0.0 or newer is required.")
+    sys.exit(1)
+
+parser = argparse.ArgumentParser(description='Segment blood vessels with multi-scale Hessian-based measure.')
+parser.add_argument('input_image')
+parser.add_argument('output_image')
+parser.add_argument('--sigma_minimum', type=float, default=1.0)
+parser.add_argument('--sigma_maximum', type=float, default=10.0)
+parser.add_argument('--number_of_sigma_steps', type=int, default=10)
+args = parser.parse_args()
+
+input_image = itk.imread(args.input_image, itk.F)
+
+ImageType = type(input_image)
+Dimension = input_image.GetImageDimension()
+HessianPixelType = itk.SymmetricSecondRankTensor[itk.D, Dimension]
+HessianImageType = itk.Image[HessianPixelType, Dimension]
+
+objectness_filter = itk.HessianToObjectnessMeasureImageFilter[HessianImageType, ImageType].New()
+objectness_filter.SetBrightObject(False)
+objectness_filter.SetScaleObjectnessMeasure(False)
+objectness_filter.SetAlpha(0.5)
+objectness_filter.SetBeta(1.0)
+objectness_filter.SetGamma(5.0)
+
+multi_scale_filter = itk.MultiScaleHessianBasedMeasureImageFilter[ImageType, HessianImageType, ImageType].New()
+multi_scale_filter.SetInput(input_image)
+multi_scale_filter.SetHessianToMeasureFilter(objectness_filter)
+multi_scale_filter.SetSigmaStepMethodToLogarithmic()
+multi_scale_filter.SetSigmaMinimum(args.sigma_minimum)
+multi_scale_filter.SetSigmaMaximum(args.sigma_maximum)
+multi_scale_filter.SetNumberOfSigmaSteps(args.number_of_sigma_steps)
+
+OutputPixelType = itk.UC
+OutputImageType = itk.Image[OutputPixelType, Dimension]
+
+rescale_filter = itk.RescaleIntensityImageFilter[ImageType, OutputImageType].New()
+rescale_filter.SetInput(multi_scale_filter)
+
+itk.imwrite(rescale_filter.GetOutput(), args.output_image)
