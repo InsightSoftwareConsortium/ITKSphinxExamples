6e06da8a5bc3cd165e4009cae753cd291e867ec9
Hans J. Johnson <hans-johnson@uiowa.edu>
2019-05-25 08:14:27 -0500

    COMP: Must remove incomplete ITKDoxygenTAG_DIR
    The rename "${ITKDoxygenTAG_TEMP_DIR}" "${ITKDoxygenTAG_DIR}" command
    failed if  "${ITKDoxygenTAG_DIR}" existed without the proper structure.

--- CMake/ITKDoxygenTAG.cmake
+++ CMake/ITKDoxygenTAG.cmake
@@ -6,8 +6,11 @@ find_package(PythonInterp REQUIRED)
 add_custom_command( OUTPUT ${ITKDoxygenTAG_DIR}/InsightDoxygen.tag
   COMMAND ${CMAKE_COMMAND} -DITKDoxygenTAG_TEMP_DIR="${ITKDoxygenTAG_TEMP_DIR}" -P ${CMAKE_CURRENT_SOURCE_DIR}/CMake/DownloadDoxygenTAG.cmake
   COMMAND ${PYTHON_EXECUTABLE} "${ITKExamples_SOURCE_DIR}/Utilities/GUnzip.py" "${ITKDoxygenTAG_TEMP_DIR}/InsightDoxygen.tag.gz"
+
+  COMMAND ${CMAKE_COMMAND} -E remove_directory "${ITKDoxygenTAG_DIR}"
   COMMAND ${CMAKE_COMMAND} -E rename "${ITKDoxygenTAG_TEMP_DIR}" "${ITKDoxygenTAG_DIR}"
   COMMAND ${CMAKE_COMMAND} -E remove_directory "${ITKDoxygenTAG_TEMP_DIR}"
+
   COMMENT "Downloading and unpacking the Doxygen TAG"
   )
 add_custom_target( ITKDoxygenTAG ${CMAKE_COMMAND} -E echo "Finished obtaining Doxygen TAG"
