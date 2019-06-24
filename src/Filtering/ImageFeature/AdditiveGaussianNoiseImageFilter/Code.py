c33ac8ba4cc45cb18981be369be368df61052638
Ben Wilson <ben.wilson@kitware.com>
2019-06-24 08:46:48 -0400

    ENH: Add AdditiveGaussianNoiseImageFilter example
    Add AdditiveGaussianNoiseImageFilter example.

--- src/Filtering/ImageFeature/AdditiveGaussianNoiseImageFilter/Code.py
+++ src/Filtering/ImageFeature/AdditiveGaussianNoiseImageFilter/Code.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
-import itk
+
+# Copyright NumFOCUS
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import sys
+import itk
 
 if len(sys.argv) != 5:
     print("Usage: " + sys.argv[0] + "< Input Image > < Output Image > [Mean] [Standard Deviation]")
@@ -11,7 +26,7 @@ outputImage = sys.argv[2]
 mean = float(sys.argv[3])
 deviation = float(sys.argv[4])
 
-# Unsigned Char for png usage
+# Use unsigned char to save to PNG format
 InputPixelType = itk.UC
 OutputPixelType = itk.UC
 Dimension = 2
@@ -28,8 +43,6 @@ AdditiveFilter = FilterType.New()
 AdditiveFilter.SetInput(reader.GetOutput())
 AdditiveFilter.SetMean(mean)
 AdditiveFilter.SetStandardDeviation(deviation)
-
-
 
 WriterType = itk.ImageFileWriter[OutputImageType]
 writer = WriterType.New()
