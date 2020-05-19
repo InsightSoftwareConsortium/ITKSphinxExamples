305801d7233688e8227350b51ded2fdc4bd7a565
Hans Johnson <hans-johnson@uiowa.edu>
2020-05-19 10:44:05 -0500

    STYLE: Format to adhere to ITK style.

--- src/Filtering/ImageFeature/AdditiveGaussianNoiseImageFilter/Code.cxx
+++ src/Filtering/ImageFeature/AdditiveGaussianNoiseImageFilter/Code.cxx
@@ -21,17 +21,18 @@
 #include "itkImageFileWriter.h"
 #include "itkAdditiveGaussianNoiseImageFilter.h"
 
-int main( int argc, char* argv[] )
+int
+main(int argc, char * argv[])
 {
   // Check for proper arguments; if not, explain usage.
-  if( argc != 5 )
-    {
-    std::cerr << "Usage: "<< std::endl;
+  if (argc != 5)
+  {
+    std::cerr << "Usage: " << std::endl;
     std::cerr << argv[0];
     std::cerr << " <InputFileName> <OutputFileName> [Mean] [Standard Deviation]";
     std::cerr << std::endl;
     return EXIT_FAILURE;
-    }
+  }
 
   // Initialize and assign user provided variables
   const char * inputImage = argv[1];
@@ -44,38 +45,38 @@ int main( int argc, char* argv[] )
   constexpr unsigned int Dimension = 2;
   // Use unsigned char to save to PNG format
   using PixelType = unsigned char;
-  using ImageType = itk::Image< PixelType, Dimension >;
+  using ImageType = itk::Image<PixelType, Dimension>;
 
   // Read the file to be converted
-  using ReaderType = itk::ImageFileReader< ImageType >;
+  using ReaderType = itk::ImageFileReader<ImageType>;
   ReaderType::Pointer reader = ReaderType::New();
-  reader->SetFileName( inputImage );
+  reader->SetFileName(inputImage);
 
-  using ImageType = itk::Image< PixelType, Dimension >;
+  using ImageType = itk::Image<PixelType, Dimension>;
 
   // Create the filter and apply the algorithm to the image
-  using FilterType = itk::AdditiveGaussianNoiseImageFilter< ImageType, ImageType >;
+  using FilterType = itk::AdditiveGaussianNoiseImageFilter<ImageType, ImageType>;
   FilterType::Pointer filter = FilterType::New();
-  filter->SetInput( reader->GetOutput() );
-  filter->SetMean( mean ); // Set the mean
-  filter->SetStandardDeviation( deviation ); // Set the standard deviation
+  filter->SetInput(reader->GetOutput());
+  filter->SetMean(mean);                   // Set the mean
+  filter->SetStandardDeviation(deviation); // Set the standard deviation
 
   // Set the writer to save file
-  using WriterType = itk::ImageFileWriter< ImageType >;
+  using WriterType = itk::ImageFileWriter<ImageType>;
   WriterType::Pointer writer = WriterType::New();
-  writer->SetFileName( outputImage );
-  writer->SetInput( filter->GetOutput() );
+  writer->SetFileName(outputImage);
+  writer->SetInput(filter->GetOutput());
 
   // Write the output image
   try
-    {
+  {
     writer->Update();
-    }
-  catch( itk::ExceptionObject & error )
-    {
+  }
+  catch (itk::ExceptionObject & error)
+  {
     std::cerr << "Error: " << error << std::endl;
     return EXIT_FAILURE;
-    }
+  }
 
   return EXIT_SUCCESS;
 }
