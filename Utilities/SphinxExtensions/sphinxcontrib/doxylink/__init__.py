1426ac93bfc60abb34624fb1639b7e69efcaaf86
Matt McCormick <matt.mccormick@kitware.com>
2020-06-01 16:56:19 -0400

    COMP: Update doxylink for recent Sphinx
    From: https://github.com/sphinx-contrib/doxylink.git
    Commit: 58c171bb5077f615dc36fc55f470a462e56da891
    
    With fixes to the remove the relative __version__ import.

--- Utilities/SphinxExtensions/sphinxcontrib/doxylink/__init__.py
+++ Utilities/SphinxExtensions/sphinxcontrib/doxylink/__init__.py
@@ -1,2 +1,6 @@
-from doxylink import *
+from .doxylink import __version__
 
+def setup(app):
+    from .doxylink import setup_doxylink_roles
+    app.add_config_value('doxylink', {}, 'env')
+    app.connect('builder-inited', setup_doxylink_roles)
