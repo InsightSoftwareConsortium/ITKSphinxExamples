e4078190718ffa0890b1807ebb95294b351957c3
Mathew Seng <mathewseng@gmail.com>
2021-01-22 15:43:48 -0600

    ENH: Create Vector Dot Product Python Example
    This example will do vector dot product arithmetic using itk's python package. In addition it will update the documentation and create a test.

---
+++ src/Core/Common/VectorDotProduct/Code.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+# ==========================================================================
+#
+#   Copyright NumFOCUS
+#
+#   Licensed under the Apache License, Version 2.0 (the "License")
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#          http://www.apache.org/licenses/LICENSE-2.0.txt
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ==========================================================================*/
+
+import itk
+from numpy.core import double
+
+Dimension = 3
+CoordType = itk.D
+
+VectorType = itk.Vector[CoordType, Dimension]
+
+u = VectorType()
+u[0] = -1.
+u[1] = 1.
+u[2] = -1.
+
+v = VectorType()
+v[0] = 1.
+v[1] = 2.
+v[2] = 3.
+
+print("u: " + str(u))
+print("v: " + str(v))
+print("DotProduct( u, v ) = " + str(u * v))
+print("DotProduct( u, v ) = " + str(u - double(u * v) * v))
