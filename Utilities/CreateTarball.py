37da5d6c993c2ee988950c20fb32dde95d0779ca
Stephen Aylward <stephen@aylward.org>
2021-03-17 21:57:15 -0400

    BUG: CreateTarball assumes /src/ is unique in path
    If src appears earlier in path, it is returned by index instead
    of the src within the SphinxExample dir.    This change updates
    the index to instead search for SphinxExample.

--- Utilities/CreateTarball.py
+++ Utilities/CreateTarball.py
@@ -24,7 +24,7 @@ if len(sys.argv) != 3:
 cwd = os.getcwd()
 example_name = sys.argv[1]
 example_dir = os.path.join(cwd, example_name)
-example_idx = example_dir.index('/src/') + 1
+example_idx = example_dir.index('SphinxExamples') + 15
 html_output_dir = os.path.join(sys.argv[2], 'html', example_dir[example_idx:])
 
 if not os.path.exists( html_output_dir ):
