2d30a5bcc45917caf3061198772c068c194fa298
Tom Birdsong <tom.birdsong@kitware.com>
2021-03-23 15:25:10 -0400

    BUG: Use GZip for Doxygen XML archive

--- CMake/DownloadDoxygenXML.cmake
+++ CMake/DownloadDoxygenXML.cmake
@@ -1,3 +1,3 @@
 file( DOWNLOAD https://itk.org/files/NightlyDoxygen/InsightDoxygenDocXml.tar.gz
-  ${ITKDoxygenXML_TEMP_DIR}/itk-doxygen-xml.tar.bz2 SHOW_PROGRESS
+  ${ITKDoxygenXML_TEMP_DIR}/itk-doxygen-xml.tar.gz SHOW_PROGRESS
   )
