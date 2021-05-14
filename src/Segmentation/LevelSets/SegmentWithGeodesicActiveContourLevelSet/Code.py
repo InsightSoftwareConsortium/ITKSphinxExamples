594ae039423e8b66db69efdab81ccf3558c886c6
Matt McCormick <matt.mccormick@kitware.com>
2021-05-14 17:44:19 -0400

    DOC: Add notebook example for SegmentWithGeodesicActiveContourLevelSet
    Also update the Python code style to be more Pythonic

--- src/Segmentation/LevelSets/SegmentWithGeodesicActiveContourLevelSet/Code.py
+++ src/Segmentation/LevelSets/SegmentWithGeodesicActiveContourLevelSet/Code.py
@@ -25,169 +25,95 @@ if len(sys.argv) != 11:
     )
     sys.exit(1)
 
-inputFileName = sys.argv[1]
+input_filename = sys.argv[1]
+output_filename = sys.argv[2]
 
+seed_pos_x = int(sys.argv[3])
+seed_pos_y = int(sys.argv[4])
 
-outputFileName = sys.argv[2]
-seedPosX = int(sys.argv[3])
-seedPosY = int(sys.argv[4])
-
-initialDistance = float(sys.argv[5])
+initial_distance = float(sys.argv[5])
 sigma = float(sys.argv[6])
 alpha = float(sys.argv[7])
 beta = float(sys.argv[8])
-propagationScaling = float(sys.argv[9])
-numberOfIterations = int(sys.argv[10])
-seedValue = -initialDistance
+propagation_scaling = float(sys.argv[9])
+number_of_iterations = int(sys.argv[10])
+seed_value = -initial_distance
 
-Dimension = 2
+InputPixelType = itk.ctype("float")
+input_image = itk.imread(input_filename, InputPixelType)
 
-InputPixelType = itk.F
-OutputPixelType = itk.UC
+Dimension = input_image.GetImageDimension()
 
-InputImageType = itk.Image[InputPixelType, Dimension]
-OutputImageType = itk.Image[OutputPixelType, Dimension]
+smoothed = itk.curvature_anisotropic_diffusion_image_filter(
+    input_image, time_step=0.125, number_of_iterations=5, conductance_parameter=9.0
+)
 
-ReaderType = itk.ImageFileReader[InputImageType]
-WriterType = itk.ImageFileWriter[OutputImageType]
+gradient_magnitude = itk.gradient_magnitude_recursive_gaussian_image_filter(
+    smoothed, sigma=sigma
+)
 
-reader = ReaderType.New()
-reader.SetFileName(inputFileName)
+sigmoid = itk.sigmoid_image_filter(
+    gradient_magnitude, output_minimum=0.0, output_maximum=1.0, alpha=alpha, beta=beta
+)
 
-SmoothingFilterType = itk.CurvatureAnisotropicDiffusionImageFilter[
-    InputImageType, InputImageType
-]
-smoothing = SmoothingFilterType.New()
-smoothing.SetTimeStep(0.125)
-smoothing.SetNumberOfIterations(5)
-smoothing.SetConductanceParameter(9.0)
-smoothing.SetInput(reader.GetOutput())
-
-GradientFilterType = itk.GradientMagnitudeRecursiveGaussianImageFilter[
-    InputImageType, InputImageType
-]
-gradientMagnitude = GradientFilterType.New()
-gradientMagnitude.SetSigma(sigma)
-gradientMagnitude.SetInput(smoothing.GetOutput())
-
-SigmoidFilterType = itk.SigmoidImageFilter[InputImageType, InputImageType]
-sigmoid = SigmoidFilterType.New()
-sigmoid.SetOutputMinimum(0.0)
-sigmoid.SetOutputMaximum(1.0)
-sigmoid.SetAlpha(alpha)
-sigmoid.SetBeta(beta)
-sigmoid.SetInput(gradientMagnitude.GetOutput())
-
-FastMarchingFilterType = itk.FastMarchingImageFilter[InputImageType, InputImageType]
-fastMarching = FastMarchingFilterType.New()
-
-GeoActiveContourFilterType = itk.GeodesicActiveContourLevelSetImageFilter[
-    InputImageType, InputImageType, InputPixelType
-]
-geodesicActiveContour = GeoActiveContourFilterType.New()
-geodesicActiveContour.SetPropagationScaling(propagationScaling)
-geodesicActiveContour.SetCurvatureScaling(1.0)
-geodesicActiveContour.SetAdvectionScaling(1.0)
-geodesicActiveContour.SetMaximumRMSError(0.02)
-geodesicActiveContour.SetNumberOfIterations(numberOfIterations)
-geodesicActiveContour.SetInput(fastMarching.GetOutput())
-geodesicActiveContour.SetFeatureImage(sigmoid.GetOutput())
-
-ThresholdingFilterType = itk.BinaryThresholdImageFilter[InputImageType, OutputImageType]
-thresholder = ThresholdingFilterType.New()
-thresholder.SetLowerThreshold(-1000.0)
-thresholder.SetUpperThreshold(0.0)
-thresholder.SetOutsideValue(itk.NumericTraits[OutputPixelType].min())
-thresholder.SetInsideValue(itk.NumericTraits[OutputPixelType].max())
-thresholder.SetInput(geodesicActiveContour.GetOutput())
-
-seedPosition = itk.Index[Dimension]()
-seedPosition[0] = seedPosX
-seedPosition[1] = seedPosY
-
-node = itk.LevelSetNode[InputPixelType, Dimension]()
-node.SetValue(seedValue)
-node.SetIndex(seedPosition)
-
+Dimension = input_image.GetImageDimension()
 seeds = itk.VectorContainer[itk.UI, itk.LevelSetNode[InputPixelType, Dimension]].New()
 seeds.Initialize()
+
+seed_position = itk.Index[Dimension]()
+seed_position[0] = seed_pos_x
+seed_position[1] = seed_pos_y
+node = itk.LevelSetNode[InputPixelType, Dimension]()
+node.SetValue(-5.0)
+node.SetIndex(seed_position)
 seeds.InsertElement(0, node)
 
-fastMarching.SetTrialPoints(seeds)
-fastMarching.SetSpeedConstant(1.0)
-
-CastFilterType = itk.RescaleIntensityImageFilter[InputImageType, OutputImageType]
-
-caster1 = CastFilterType.New()
-caster2 = CastFilterType.New()
-caster3 = CastFilterType.New()
-caster4 = CastFilterType.New()
-
-writer1 = WriterType.New()
-writer2 = WriterType.New()
-writer3 = WriterType.New()
-writer4 = WriterType.New()
-
-caster1.SetInput(smoothing.GetOutput())
-writer1.SetInput(caster1.GetOutput())
-writer1.SetFileName("GeodesicActiveContourImageFilterOutput1.png")
-caster1.SetOutputMinimum(itk.NumericTraits[OutputPixelType].min())
-caster1.SetOutputMaximum(itk.NumericTraits[OutputPixelType].max())
-writer1.Update()
-
-caster2.SetInput(gradientMagnitude.GetOutput())
-writer2.SetInput(caster2.GetOutput())
-writer2.SetFileName("GeodesicActiveContourImageFilterOutput2.png")
-caster2.SetOutputMinimum(itk.NumericTraits[OutputPixelType].min())
-caster2.SetOutputMaximum(itk.NumericTraits[OutputPixelType].max())
-writer2.Update()
-
-caster3.SetInput(sigmoid.GetOutput())
-writer3.SetInput(caster3.GetOutput())
-writer3.SetFileName("GeodesicActiveContourImageFilterOutput3.png")
-caster3.SetOutputMinimum(itk.NumericTraits[OutputPixelType].min())
-caster3.SetOutputMaximum(itk.NumericTraits[OutputPixelType].max())
-writer3.Update()
-
-caster4.SetInput(fastMarching.GetOutput())
-writer4.SetInput(caster4.GetOutput())
-writer4.SetFileName("GeodesicActiveContourImageFilterOutput4.png")
-caster4.SetOutputMinimum(itk.NumericTraits[OutputPixelType].min())
-caster4.SetOutputMaximum(itk.NumericTraits[OutputPixelType].max())
-
-fastMarching.SetOutputSize(reader.GetOutput().GetBufferedRegion().GetSize())
-
-writer = WriterType.New()
-writer.SetFileName(outputFileName)
-writer.SetInput(thresholder.GetOutput())
-writer.Update()
-
-print(
-    "Max. no. iterations: " + str(geodesicActiveContour.GetNumberOfIterations()) + "\n"
+fast_marching = itk.fast_marching_image_filter(
+    trial_points=seeds,
+    speed_constant=1.0,
+    output_size=input_image.GetBufferedRegion().GetSize(),
 )
-print("Max. RMS error: " + str(geodesicActiveContour.GetMaximumRMSError()) + "\n")
-print(
-    "No. elpased iterations: "
-    + str(geodesicActiveContour.GetElapsedIterations())
-    + "\n"
+
+geodesic_active_contour = itk.geodesic_active_contour_level_set_image_filter(
+    fast_marching,
+    propagation_scaling=propagation_scaling,
+    curvature_scaling=1.0,
+    advection_scaling=1.0,
+    maximum_r_m_s_error=0.02,
+    number_of_iterations=number_of_iterations,
+    feature_image=sigmoid,
 )
-print("RMS change: " + str(geodesicActiveContour.GetRMSChange()) + "\n")
 
-writer4.Update()
 
-InternalWriterType = itk.ImageFileWriter[InputImageType]
+OutputPixelType = itk.ctype("unsigned char")
+OutputImageType = itk.Image[OutputPixelType, Dimension]
+thresholded = itk.binary_threshold_image_filter(
+    geodesic_active_contour,
+    lower_threshold=-1000.0,
+    upper_threshold=0.0,
+    outside_value=itk.NumericTraits[OutputPixelType].min(),
+    inside_value=itk.NumericTraits[OutputPixelType].max(),
+    ttype=[type(geodesic_active_contour), OutputImageType],
+)
 
-mapWriter = InternalWriterType.New()
-mapWriter.SetInput(fastMarching.GetOutput())
-mapWriter.SetFileName("GeodesicActiveContourImageFilterOutput4.mha")
-mapWriter.Update()
+itk.imwrite(thresholded, output_filename)
 
-speedWriter = InternalWriterType.New()
-speedWriter.SetInput(sigmoid.GetOutput())
-speedWriter.SetFileName("GeodesicActiveContourImageFilterOutput3.mha")
-speedWriter.Update()
+smoothed_cast = itk.rescale_intensity_image_filter(
+    smoothed, ttype=(type(smoothed), OutputImageType)
+)
+itk.imwrite(smoothed_cast, "GeodesicActiveContourImageFilterOutput1.png")
 
-gradientWriter = InternalWriterType.New()
-gradientWriter.SetInput(gradientMagnitude.GetOutput())
-gradientWriter.SetFileName("GeodesicActiveContourImageFilterOutput2.mha")
-gradientWriter.Update()
+gradient_magnitude_cast = itk.rescale_intensity_image_filter(
+    smoothed, ttype=(type(gradient_magnitude), OutputImageType)
+)
+itk.imwrite(gradient_magnitude_cast, "GeodesicActiveContourImageFilterOutput2.png")
+
+sigmoid_cast = itk.rescale_intensity_image_filter(
+    sigmoid, ttype=(type(sigmoid), OutputImageType)
+)
+itk.imwrite(sigmoid_cast, "GeodesicActiveContourImageFilterOutput3.png")
+
+fast_marching_cast = itk.rescale_intensity_image_filter(
+    fast_marching, ttype=(type(fast_marching), OutputImageType)
+)
+itk.imwrite(fast_marching_cast, "GeodesicActiveContourImageFilterOutput4.png")
