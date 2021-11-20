e47429943bf276ff11fea6c300a114277eb122e2
Hans Johnson <hans-johnson@uiowa.edu>
2021-11-20 12:16:53 -0600

    STYLE: Remove python style warnings
    Shadow variable warnings, unused variable warnings
    addressed.

--- Utilities/CookieCutter/{{cookiecutter.example_name}}/Code.py
+++ Utilities/CookieCutter/{{cookiecutter.example_name}}/Code.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
+import argparse
 import itk
 
 parser = argparse.ArgumentParser(description="{{ cookiecutter.example_title }}.")
