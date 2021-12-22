be9d4e691b3325b5d3108b38a30efa192db48075
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2021-12-21 20:32:56 -0500

    BUG: Inherit `ImageSource` from `ProcessObject`
    Inherit `ImageSource` from `ProcessObject`: the class is meant to serve
    as a data source/generator, not a filter.

--- src/Developer/ImageSource.h
+++ src/Developer/ImageSource.h
@@ -1,26 +1,26 @@
 #ifndef __itkImageFilter_h
 #define __itkImageFilter_h
 
-#include "itkImageToImageFilter.h"
+#include "itkProcessObject.h"
 
 #include <set>
 
 namespace itk
 {
 template <class TImage>
-class ImageSource : public ImageToImageFilter<TImage, TImage>
+class ImageSource : public ProcessObject
 {
 public:
   /** Standard class type alias. */
   using Self = ImageSource;
-  using Superclass = ImageToImageFilter<TImage, TImage>;
+  using Superclass = ProcessObject;
   using Pointer = SmartPointer<Self>;
 
   /** Method for creation through the object factory. */
   itkNewMacro(Self);
 
   /** Run-time type information (and related methods). */
-  itkTypeMacro(ImageSource, ImageToImageFilter);
+  itkTypeMacro(ImageSource, ProcessObject);
 
 protected:
   ImageSource() {}
