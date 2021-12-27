InsightSoftwareConsortium/ITKSphinxExamples

commit 89a73005fac92e1364c2e3e3ccd7492c99fb446f

STYLE: address unused variable warnings.
Types and variables that were unused were
removed to avoid compiler warnings.

src/Filtering/LabelMap/ShapeAttributesForBinaryImage/Code.cxx
@@ -22,10 +22,10 @@
 
 template <typename TImage>
 static void
-CreateImage(TImage * const image);
+CreateImage(TImage * image);
 
 int
-main(int argc, char * argv[])
+main(int argc, char * [])
 {
   constexpr unsigned int Dimension = 2;
   using PixelType = unsigned char;

src/Filtering/Thresholding/DemonstrateThresholdAlgorithms/Code.cxx
@@ -51,9 +51,13 @@ main(int argc, char * argv[])
   }
 #ifdef ENABLE_QUICKVIEW
   using InputPixelType = short;
-  using OutputPixelType = unsigned char;
 
   using InputImageType = itk::Image<InputPixelType, 2>;
+
+  const auto input = itk::ReadImage<InputImageType>(argv[1]);
+
+#ifdef ENABLE_QUICKVIEW
+  using OutputPixelType = unsigned char;
   using OutputImageType = itk::Image<OutputPixelType, 2>;
 
   using LiFilterType = itk::LiThresholdImageFilter<InputImageType, OutputImageType>;

src/Core/Transform/GlobalRegistrationTwoImagesBSpline/Code.cxx
@@ -30,7 +30,6 @@
 #include "itkImageFileWriter.h"
 #include "itkResampleImageFilter.h"
 #include "itkCastImageFilter.h"
-#include "itkSquaredDifferenceImageFilter.h"
 
 #ifdef ENABLE_QUICKVIEW
 #  include "QuickView.h"
@@ -42,9 +41,9 @@ using PixelType = float;
 using ImageType = itk::Image<PixelType, ImageDimension>;
 
 static void
-CreateEllipseImage(ImageType::Pointer image);
+CreateEllipseImage(const ImageType::Pointer & image);
 static void
-CreateCircleImage(ImageType::Pointer image);
+CreateCircleImage(const ImageType::Pointer & image);
 
 int
 main(int itkNotUsed(argc), char * itkNotUsed(argv)[])
@@ -252,7 +251,7 @@ main(int itkNotUsed(argc), char * itkNotUsed(argv)[])
 }
 
 void
-CreateEllipseImage(ImageType::Pointer image)
+CreateEllipseImage(const ImageType::Pointer & image)
 {
   using EllipseType = itk::EllipseSpatialObject<ImageDimension>;
 
@@ -300,7 +299,7 @@ CreateEllipseImage(ImageType::Pointer image)
 }
 
 void
-CreateCircleImage(ImageType::Pointer image)
+CreateCircleImage(const ImageType::Pointer & image)
 {
   using EllipseType = itk::EllipseSpatialObject<ImageDimension>;
 

commit 02d730f043ef9b2b3e69598993607c789c561453

COMP: fix compile errors by aggressive code cleanup
The errors were introduced by 89a73005fac92e1364c2e3e3ccd7492c99fb446f.

/home/runner/work/Ex/src/Filtering/LabelMap/ShapeAttributesForBinaryImage/Code.cxx: In function 'int main(int, char**)':
/home/runner/work/Ex/src/Filtering/LabelMap/ShapeAttributesForBinaryImage/Code.cxx:48:16: error: 'argv' was not declared in this scope
     fileName = argv[1];
                ^~~~

../src/Filtering/Thresholding/DemonstrateThresholdAlgorithms/Code.cxx:52:0: error: unterminated #ifdef

src/Filtering/LabelMap/ShapeAttributesForBinaryImage/Code.cxx
@@ -25,7 +25,7 @@ static void
 CreateImage(TImage * image);
 
 int
-main(int argc, char *[])
+main(int argc, char * argv[])
 {
   constexpr unsigned int Dimension = 2;
   using PixelType = unsigned char;

src/Filtering/Thresholding/DemonstrateThresholdAlgorithms/Code.cxx
@@ -56,7 +56,6 @@ main(int argc, char * argv[])
 
   const auto input = itk::ReadImage<InputImageType>(argv[1]);
 
-#  ifdef ENABLE_QUICKVIEW
   using OutputPixelType = unsigned char;
   using OutputImageType = itk::Image<OutputPixelType, 2>;
 
@@ -112,8 +111,8 @@ main(int argc, char * argv[])
 
 
   viewer.Visualize();
-#  else
+#else
   std::cout << " Example requires compilation with ENABLE_QUICKVIEW defined." << std::endl;
-#  endif
+#endif
   return EXIT_SUCCESS;
 }