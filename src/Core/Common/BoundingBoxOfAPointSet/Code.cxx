b8f45370106cdaf17aa037387bf135f373120d3e
Hans Johnson <hans-johnson@uiowa.edu>
2024-11-25 16:09:17 -0600

    STYLE: One declaration per line for readability
    Multiple declarations in a single statement reduces readability.
    
    Detect local variable declaration statements and update to have only one
    statement per declaration.
    
    Initialize values rather than independant assignments of each
    element

--- src/Core/Common/BoundingBoxOfAPointSet/Code.cxx
+++ src/Core/Common/BoundingBoxOfAPointSet/Code.cxx
@@ -36,17 +36,11 @@ main()
   PointsContainerPointer points = pointSet->GetPoints();
 
   // Create points
-  PointType p0, p1, p2;
-
-  p0[0] = 0.0;
-  p0[1] = 0.0;
-  p0[2] = 0.0;
-  p1[0] = 0.1;
-  p1[1] = 0.0;
-  p1[2] = 0.0;
-  p2[0] = 0.0;
-  p2[1] = 0.1;
-  p2[2] = 0.0;
+  // Create points
+  using PointType = PointSetType::PointType;
+  const PointType p0({ 0.0, 0.0, 0.0 });
+  const PointType p1({ 0.1, 0.0, 0.0 });
+  const PointType p2({ 0.0, 0.1, 0.0 });
 
   points->InsertElement(0, p0);
   points->InsertElement(1, p1);
