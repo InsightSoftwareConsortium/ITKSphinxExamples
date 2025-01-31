1533a146c1c0a21142b379044854caf9718e17d4
Dženan Zukić <dzenan.zukic@kitware.com>
2025-01-31 14:11:21 -0500

    ENH: Require CMake 3.22.1 in dashboards

--- itkexamples_common.cmake
+++ itkexamples_common.cmake
@@ -91,7 +91,7 @@
 #
 #==========================================================================*/
 
-cmake_minimum_required(VERSION 2.8.6 FATAL_ERROR)
+cmake_minimum_required(VERSION 3.22.1 FATAL_ERROR)
 
 set(dashboard_user_home "$ENV{HOME}")
 
