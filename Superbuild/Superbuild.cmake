d8f0a55a309e596913fe2389eb4cadb65b85c1e1
Hans Johnson <hans-johnson@uiowa.edu>
2025-01-30 18:58:07 -0600

    ENH: Remove references to old clang-format binaries
    clang-format version 8.0.0 binaries are no longer necessary.
    There are several ways (including pip install) to get the
    clang-format version 19 that are consistent with recent
    ITK style.

--- Superbuild/Superbuild.cmake
+++ Superbuild/Superbuild.cmake
@@ -115,19 +115,3 @@ ExternalProject_Add(ITKSphinxExamples
     -DExternalData_OBJECT_STORES:STRING=${ExternalData_OBJECT_STORES}
   INSTALL_COMMAND ""
 )
-
-# Download and configure clang-format for style uniformity during development.
-#
-# This code helps download a version of clang-format binaries consistent with
-# ITK's clang-format configuration. Git configuration is added during
-# development to help automatically apply the formatting.
-#
-# Globally, the ITK/.clang-format file provides the base style configuration.
-# For more information on ITK coding style, see the ITK Coding Style Guide in
-# the ITK Software Guide.
-option(ITKSphinxExamples_USE_CLANG_FORMAT "Enable the use of clang-format for coding style formatting." ${BUILD_TESTING})
-mark_as_advanced(ITKSphinxExamples_USE_CLANG_FORMAT)
-
-if(BUILD_TESTING AND ITKSphinxExamples_USE_CLANG_FORMAT AND NOT CMAKE_CROSSCOMPILING)
-  include(${CMAKE_SOURCE_DIR}/External-ClangFormat.cmake)
-endif()
